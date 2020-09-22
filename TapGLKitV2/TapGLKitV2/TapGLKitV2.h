d643f2688d82879dc13e84415a8c97b2c5749a55
O Rabie <o.rabie@tap.company>
2020-09-22 13:38:17 +0200

    Upgrade to Xcode12+ compiler friendly code

---
+++ TapGLKitV2/TapGLKitV2/TapGLKitV2.h
@@ -0,0 +1,18 @@
+//
+//  TapGLKitV2.h
+//  TapGLKitV2
+//
+//  Created by Osama Rabie on 9/22/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TapGLKitV2.
+FOUNDATION_EXPORT double TapGLKitV2VersionNumber;
+
+//! Project version string for TapGLKitV2.
+FOUNDATION_EXPORT const unsigned char TapGLKitV2VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TapGLKitV2/PublicHeader.h>
+
+
